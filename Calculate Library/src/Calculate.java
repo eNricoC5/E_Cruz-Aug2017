b38233eb9e85f17d3413942bc76a0c3d96e9df20
eNricoC5 <cruzenrico28@gmail.com>
2017-09-05 09:29:53 -0700

    Second Project -- using Git from Eclipse

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,17 @@
+/* Enrico Cruz
+ * September 5, 2017
+* This is our home grown library that will contain various
+*mathematical methods.
+*/
+public class Calculate {
+/* This method takes an integer and returns its square
+ */
+	public static int square(int operand) {
+		return operand * operand;
+	}
+	/* This method takes an integer and returns it a square
+	 */
+	public static int cube(int operand) {
+		return operand * operand *operand
+	}
+}
---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,14 @@
+//Enrico Cruz 
+//1st period Dreyer
+//Pikachu code assignment
+public class Pikachu {
+public static void main(String[] arg) {
+
+System.out.println("Pikachu welcomes you to the world of Pokemon");
+
+System.out.println("\t\t  (\\__/)");
+System.out.println("\t\t  (o^.^)");
+System.out.println("\t\tz(_(\")(\")");
+
+}
+}
