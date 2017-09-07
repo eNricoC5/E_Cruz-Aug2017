663c14f8f13b31b8dbb95edafba1052542a563db
eNricoC5 <cruzenrico28@gmail.com>
2017-09-07 08:53:23 -0700

    I added more to this liibrary

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -7,11 +7,32 @@ public class Calculate {
 /* This method takes an integer and returns its square
  */
 	public static int square(int operand) {
-		return operand * operand;
+		int square= operand * operand;
+		return square;
 	}
 	/* This method takes an integer and returns it a square
 	 */
 	public static int cube(int operand) {
-		return operand * operand *operand
+		int cube= operand * operand * operand;
+		return cube;
+	}
+	/* This method takes an integer and cubes it
+	 */
+	public static double average(double operand, double operand2) {
+		double average= (operand + operand2)/2;
+		return average;
+	}
+	/* This method takes in 2 doubles and averages the two 
+	 */
+	public static double average2(double operand, double operand2, double operand3) {
+		double average2= (operand + operand2 + operand3)/3;
+		return average2;
+	}
+	/* This method takes in 3 doubles and averages the 3
+	 */
+	public static double toDegrees(double operand) {
+		double toDegrees= operand * (180/3.14159);
+		return toDegrees;
+	
 	}
 }
