b9e6a5f695fcfcbae58dd73bc4f56068db8036d3
eNricoC5 <cruzenrico28@gmail.com>
2017-09-12 11:45:49 -0700

    added discriminant fixed the degree and radian problem

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -7,39 +7,46 @@ public class Calculate {
 /* This method takes an integer and returns its square
  */
 	public static int square(int operand) {
-		int square= operand * operand;
-		return square;
+		int s= operand * operand;
+		return s;
 	}
 	/* This method takes an integer and returns it a square
 	 */
 	public static int cube(int operand) {
-		int cube= operand * operand * operand;
-		return cube;
+		int q= operand * operand * operand;
+		return q;
 	}
 	/* This method takes an integer and cubes it
 	 */
 	public static double average(double operand, double operand2) {
-		double average= (operand + operand2)/2;
-		return average;
+		double ave= (operand + operand2)/2;
+		return ave;
 	}
 	/* This method takes in 2 doubles and averages the two 
 	 */
 	public static double average2(double operand, double operand2, double operand3) {
-		double average2= (operand + operand2 + operand3)/3;
-		return average2;
+		double ave2= (operand + operand2 + operand3)/3;
+		return ave2;
 	}
 	/* This method takes in 3 doubles and averages the 3
 	 */
 	public static double toDegrees(double operand) {
-		double toDegrees= (operand * (3.14159/180));
-		return toDegrees;
+		double Degrees= (operand * (3.14159/180));
+		return Degrees;
 	}
 	/* This method takes in a double of a radian and converts it
 	 * to a degree
 	 */
 	public static double toRadians(double operand) {
-		double toRadians= (operand * (180 * 3.14159));
-		return toRadians;
+		double Radians= (operand * (180/3.14159));
+		return Radians;
+	}
+	/*This method takes in a double of a degree and conerts it
+	 * to a radian
+	 */
+	public static double discriminant(double operand, double operand2, double operand3) {
+		double dis= operandx^2 + operand2x + operand 3;
+		return dis;
 	}
 
  }
