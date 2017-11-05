e408f8bdece9771c19d7f21f81f461b889018a39
eNricoC5 <cruzenrico28@gmail.com>
2017-11-05 12:48:06 -0800

    Array Lab 3 Along with some other things I still don't know what it is

---
+++ Array Lab 3/src/ArraysLab3.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArraysLab3 {
+public static void main(String[] args) {
+	int[] arrX = {1,2,3,4,5};
+	int[] arrY = {2,4,6,8,10};
+sum(arrX,arrY);
+System.out.println(Arrays.toString(arrX));
+System.out.println(Arrays.toString(arrY));
+}
+	public static int[] sum(int[] arr1, int[] arr2) {
+	int[] arr3 = new int[arr1.length + arr2.length];
+	arr1 = arrX;
+	for (int i=0; i<arrX.length; i++) {
+		arrX[i];
+			}
+		}
+	}
+
+
