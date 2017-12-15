daf82b2d10a48b2a1beffe5d3738c2d2092b0a90
eNricoC5 <cruzenrico28@gmail.com>
2017-12-15 14:45:37 -0800

    Frac Calc part 3 needs a lot of work

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -39,39 +39,36 @@
      public static String produceAnswer(String input) { 
          // TODO: Implement this function to produce the solution to the input
      	String[] fullArray= input.split(" ");
-     	
+     	String[] firstOperand = new String[3];
+        String[] secondOperand = new String[3];
+        
       	String firstNum= fullArray[0];
       	String operator= fullArray[1];
       	String secondNum= fullArray[2];
-     	
-     	 String[] firstOperand = new String[3];
-         String[] secondOperand = new String[3];
-          
-         splitOperands(firstOperand, answerStringOp1);
-         splitOperands(secondOperand, answerStringOp2);
          
-         int wholeNum1 = parseInt(answerStringOp1[0]);
-         int numeratorNum1 = parseInt(answerStringOp1[1]);
-         int denominatorNum1 = parseInt(answerStringOp1[2]);
+         splitOperands(firstNum, firstOperand);
+         splitOperands(secondNum, secondOperand);
          
-         int wholeNum2 = parseInt(answerStringOp2[0]);
-         int numeratorNum2 = parseInt(answerStringOp2[1]);
-         int denominatorNum2 = parseInt(answerStringOp2[2]);
+         int wholeNum1 = Integer.parseInt(firstOperand[0]);
+         int numeratorNum1 = Integer.parseInt(firstOperand[1]);
+         int denominatorNum1 = Integer.parseInt(firstOperand[2]);
+         
+         int wholeNum2 = Integer.parseInt(secondOperand[0]);
+         int numeratorNum2 = Integer.parseInt(secondOperand[1]);
+         int denominatorNum2 = Integer.parseInt(secondOperand[2]);
          
          int impropFrac1 = toImproperFrac(wholeNum1, numeratorNum1, denominatorNum1);
          int impropFrac2 = toImproperFrac(wholeNum2, numeratorNum2, denominatorNum2);
          int[] equation;
         		 if (operator.equals("+") || operator.equals("-")) {
-        			equation=addSub(impropFrac1,improperFrac2);
+        			equation = addSub(impropFrac1,improperFrac2);
         			return equation[0] + "/" + equation[1];
         		 } else {
         			 equation = multiDiv(improperFrac1,improperFrac2);
         			 return equation[0] + "/" + equation[1];
-        			 
-        		 }
-         //return splitOperands(mathArray);
-      }	
-      public static String splitOperands(String input2) {
+        	}
+     	}	
+      public static void splitOperands(String input2) {
       	if (input2.contains("_") && (input2.contains("/"))) {
  	    	String[] firstSplit = (input2.split("_"));
  	    	String fW= firstSplit[0];
@@ -95,7 +92,7 @@
       		return wholeNum;
       	}
   	}
-     public static int addSub(String operator,int num1,int num2, int den1, int den2) {
+     public static int[] addSub(String operator,int num1,int num2, int den1, int den2) {
     	 int[] frac = new int[2];
  	 	frac[1] = den1 * den2;
  	 	if (operator.contains("+")) {
@@ -104,21 +101,28 @@
  		 frac[0] = (num1*den2) - (num2*den1);
  		 return frac;
  	 	}
+		return frac;
     }
-     public static int multiDiv(String operator,int num1,int num2, int den1, int den2) {
+     public static int[] multiDiv(String operator,int num1,int num2, int den1, int den2) {
     	 int[] frac1 = new int[2];
     	 	if (operator.contains("*")) {
     	 		frac1[0] = num1 * num2;
-    	 		frac1[1] = den1 * den 2;
+    	 		frac1[1] = den1 * den2;
     	 	} else {
     	 		frac1[0] = num1 * den2;
     	 		frac1[1] = den1 * num2;
-    	 		return frac1
+    	 		return frac1;
     	 	}
+			return frac1;
      }
-     public static String toImproperFrac(int whole, int numerator, int denominator) {
- 		int imp= (whole * denominator) + numerator;
- 		return imp + "/" + denominator;
+     public static int toImproperFrac(int whole, int numerator, int denominator) {
+    	 int imp;
+    	 if (numerator > 0) {
+ 		imp= (whole * denominator) + numerator;
+    	 } else {
+    	imp= (whole * denominator) - numerator;
+    	 }
+ 		return imp;
  	}
   }
 
